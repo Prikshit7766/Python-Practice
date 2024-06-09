5fda4279074b8ebf566f7a4227b282371d88d6e8
Prikshit7766 <prikshitsharma8024@gmail.com>
2024-06-09 18:05:57 +0530

    Building A Word Counter In Django

--- Django/myproject/myapp/urls.py
+++ Django/myproject/myapp/urls.py
@@ -3,9 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"), # this is root URL, this will call the index function from views.py 
+    path('counter_page', views.counter_page, name="counter_page"),
+    path('count', views.count, name="count"), 
 
 ]
-
 
 # suppose if the user comenst to any particular URL then what will happen ?
 # we render a html file, can can send an http or  a json response (anything) that is going to happen in the views.py file
--- Django/myproject/myapp/views.py
+++ Django/myproject/myapp/views.py
@@ -10,6 +10,24 @@ def index(request):
         'name': "Prikshit",
         'age': 22,
         'city': "jammu"
-
     }
     return render(request, "index.html", context)
+
+def counter_page(request):
+    return render(request, "counter_page.html") 
+
+def count(request):
+    text = request.GET['text']
+    words = text.split()
+    word_count = len(words)
+    return render(request, "count.html", {'text': text, 'word_count': word_count})
+
+
+def count(request):
+    if request.method == 'POST':
+        text = request.POST.get('text', '')
+        words = text.split()
+        word_count = len(words)
+        return render(request, "count.html", {'text': text, 'word_count': word_count})
+    else:
+        return HttpResponse("Method not allowed")
