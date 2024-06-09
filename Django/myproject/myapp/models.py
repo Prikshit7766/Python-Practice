031136a1860a7ec2c8a5d3ba13caeef7dc25c096
Prikshit7766 <prikshitsharma8024@gmail.com>
2024-06-10 00:23:08 +0530

    Introduction To Django Models

--- Django/myproject/myapp/models.py
+++ Django/myproject/myapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+# mostly used in configuring the database. we mostly don't need to write single line of sql code to get your database up and running, that what we have models vews templates
+
+# from the mode we just pass all our data into templates file
+
+class Feature:
+    id: int
+    name: str
+    detail:str
+    is_true: bool
+    icon: str
+
+    
+    def __str__(self):
+        return self.name
--- Django/myproject/myapp/views.py
+++ Django/myproject/myapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Feature
 
 # Create your views here.
 
@@ -33,4 +34,35 @@ def count(request):
         return HttpResponse("Method not allowed")
     
 def index_demo(request):
-    return render(request, "index_demo.html")
+    # create 4 features 
+    feature1 = Feature()
+    feature1.id = 1
+    feature1.name = "Fast"
+    feature1.detail = "This is a fast feature"
+    feature1.is_true = True
+    feature1.icon = "bi bi-gem"
+
+    feature2 = Feature()
+    feature2.id = 2
+    feature2.name = "Secure"
+    feature2.detail = "This is a secure feature"
+    feature2.is_true = True
+    feature2.icon = "bi bi-lock"
+
+    feature3 = Feature()
+    feature3.id = 3
+    feature3.name = "Reliable"
+    feature3.detail = "This is a reliable feature"
+    feature3.is_true = False
+    feature3.icon = "bi bi-book"
+
+    feature4 = Feature()
+    feature4.id = 4
+    feature4.name = "Scalable"
+    feature4.detail = "This is a scalable feature"
+    feature4.is_true = True
+    feature4.icon = "bi bi-arrows-expand"
+
+    features = [feature1, feature2, feature3, feature4]
+
+    return render(request, "index_demo.html", {'features': features})
